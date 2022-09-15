de244e47e4a0a039c4c899902f06bee89ff1af1d
msdvnet <110641210+msdvnet@users.noreply.github.com>
2022-09-14 22:34:35 -0700

    Create install.py
    Signed-off-by: msdvnet <110641210+msdvnet@users.noreply.github.com>

---
+++ install.py
@@ -0,0 +1,8 @@
+#!/bin/env python
+# Initalize variables for installer
+listOS = '';
+# Installer title
+print('AutoAmp - The only all-in-one LAMP installer')
+# Show user a list of operating systems for LAMP install
+print('Choose a your OS for LAMP installation: ')
+
